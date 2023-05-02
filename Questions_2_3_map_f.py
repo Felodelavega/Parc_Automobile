c96fe78acc9d5ac481378fa3e823ca9d7812f4e1
FranckA <hyper1connu@gmail.com>
2023-05-02 22:38:42 +0200

    Update Questions_2_3_map_f.py

--- Questions_2_3_map_f.py
+++ Questions_2_3_map_f.py
@@ -17,34 +17,34 @@ p = os.getcwd()
 
 # , dtype={3: str, 4: str})
 df_propre = pd.read_csv(
-    # p + r'/France_data/df_merged_geoloc_lat_long.csv', index_col=[0], dtype={4: str, 5: str, 21: str})
+#    p + r'/France_df_commune_unique/df_commune_merged_geoloc_lat_long.csv', index_col=[0], dtype={4: str, 5: str, 21: str})
     p + r'/France_data/parc_vp_propre_geoloc_final.csv', index_col=[0], dtype={1: str, 2: str, 20: str})
 df_propre[['lattitude', 'longitude']
           ] = df_propre['geo_point_2d'].str.split(',', expand=True)
 df_propre[['lattitude', 'longitude']] = df_propre[[
     'lattitude', 'longitude']].astype(float)
 
-def carto(df,model):
+def carto(df_commune,model):
     # init google map
-    lat0 = df.iloc[0]['lattitude']
-    lon0 = df.iloc[0]['longitude']
+    lat0 = df_commune.iloc[0]['lattitude']
+    lon0 = df_commune.iloc[0]['longitude']
     
     # instanciation objets
     gmap = gmplot.GoogleMapPlotter(lat0, lon0, 13)
 
     m = folium.Map(location=[lat0, lon0], zoom_start=15)
 
-    for commune in df['commune_de_residence'].unique():
-        data = df[df['commune_de_residence'] == commune]
-        # data = df[df['commune_de_residence'].isin(commune)]
-        print ('for commune',df)
-        lat0 = data.iloc[0]['lattitude']
-        lon0 = data.iloc[0]['longitude']
-        shape = data.iloc[0]['geo_shape']
-        shape_json = json.loads(shape)
+    for commune in df_commune['commune_de_residence'].unique():
+        df_commune_unique = df_commune[df_commune['commune_de_residence'] == commune]
+        # df_commune_unique = df_commune[df_commune['commune_de_residence'].isin(commune)]
+        print ('for commune',df_commune)
+        lat0 = df_commune_unique.iloc[0]['lattitude']
+        lon0 = df_commune_unique.iloc[0]['longitude']
+        shape_commune = df_commune_unique.iloc[0]['geo_shape']
+        shape_commune_json = json.loads(shape_commune)
            
         # Récupération des coordonnées du polygone
-        polygon_coords = shape_json['coordinates'][0]
+        polygon_coords = shape_commune_json['coordinates'][0]
 
         # Séparation des coordonnées en deux listes: lattitudes et longitudes
         lats = [coord[1] for coord in polygon_coords]
@@ -54,24 +54,24 @@ def carto(df,model):
 
         # Tracé du polygone sur la carte
         gmap.polygon(lats, lngs)
-        folium.GeoJson(data=shape_json).add_to(m)
+        folium.GeoJson(data=shape_commune_json).add_to(m)
            
         plt.figure(figsize=(2.5, 2.5))
         # Change the font size of the pie chart labels
         plt.rcParams['font.size'] = 8
 
-        data = data.reset_index(drop=True)
-        # print('data', data)
+        df_commune_unique = df_commune_unique.reset_index(drop=True)
+        # print('df_commune_unique', df_commune_unique)
 
-        explode_index = data['proportion'].idxmax()
+        explode_index = df_commune_unique['proportion'].idxmax()
 
         # Create an "explode" array with a non-zero value for the largest percentage
-        explode = [0.1 if i == explode_index else 0 for i in range(len(data))]
+        explode = [0.1 if i == explode_index else 0 for i in range(len(df_commune_unique))]
 
-        print (data['couleur'])
+        # print (df_commune_unique['couleur'])
 
-        plt.pie(data['proportion'], labels=data[model],autopct='%1.1f%%',
-                colors=data['couleur'], explode=explode)
+        plt.pie(df_commune_unique['proportion'], labels=df_commune_unique[model],autopct='%1.1f%%',
+                colors=df_commune_unique['couleur'], explode=explode)
         plt.title(commune)
 
         # Save the plot to a BytesIO object
@@ -85,7 +85,7 @@ def carto(df,model):
         # Create an HTML img tag using the base64 string
         html = f'<img src="data:image/png;base64,{encoded}">'
  
-        # Add a marker for each location in the dfFrame
+        # Add a marker for each location in the df_communeFrame
         gmap.marker(lat0, lon0, info_window=html)
         marker_cluster = MarkerCluster().add_to(m)
         # Create a folium Popup object containing the HTML img tag
@@ -117,42 +117,42 @@ def y_formatter(y, pos):
 # Afficher la variation du nb de VL par crit'air ou par carburant en fonction de la region, du departement, des communes, du type d'energie et de la plage d'annees
 
 
-def sort_par_model_couleur(df,model):
+def sort_par_model_couleur(df_commune,model):
     if model == 'crit_air':
         model_order = {'Crit\'Air E': 0, 'Crit\'Air 1': 1, 'Crit\'Air 2': 2,
                         'Crit\'Air 3': 3, 'Crit\'Air 4': 4, 'Crit\'Air 5': 5, 'Non classé': 6, 'Inconnu': 7}
-        df = df.sort_values(by=[model,'Annee'], key=lambda x: x.map(model_order))
+        df_commune = df_commune.sort_values(by=[model,'Annee'], key=lambda x: x.map(model_order))
         
         # Define colors for each Crit'Air category
         colors = {"Crit'Air E": 'green', "Crit'Air 1": 'purple', "Crit'Air 2": 'yellow', "Crit'Air 3": 'orange',
         "Crit'Air 4": 'maroon', "Crit'Air 5": 'gray', "Inconnu": 'black', "Non classé": 'brown'}
         
         # Add color column based on Crit'Air category
-        df['couleur'] = [colors[x] for x in df[model]]
-        return df.reset_index(drop=True)
+        df_commune['couleur'] = [colors[x] for x in df_commune[model]]
+        return df_commune.reset_index(drop=True)
     
     elif model == 'carburant':
         model_order = {'Electrique et hydrogène': 0, 'Hybride rechargeable': 1, 'Gaz et inconnu': 2,
                         'Essence HNR': 3, 'Diesel HNR': 4, 'Essence': 5, 'Diesel': 6}
-        df = df.sort_values(by=[model,'Annee'], key=lambda x: x.map(model_order))
+        df_commune = df_commune.sort_values(by=[model,'Annee'], key=lambda x: x.map(model_order))
 
         # Define colors for each Crit'Air category
         colors = {"Electrique et hydrogène": 'green', "Hybride rechargeable": 'purple', "Gaz et inconnu": 'yellow', "Essence HNR": 'orange',
         "Diesel HNR": 'maroon', "Essence": 'gray', "Diesel": 'black'}
         
         # Add color column based on Crit'Air category
-        df['couleur'] = [colors[x] for x in df[model]]
+        df_commune['couleur'] = [colors[x] for x in df_commune[model]]
 
-        return df.reset_index(drop=True)
+        return df_commune.reset_index(drop=True)
     else: print ('model inconnu')
 
 
 
 
-def plot_vehicule_evolution(df, model, region=None, departement=None, commune=None, carburant=None, crit_air=None, depart_Annee=None, fin_Annee=None):
+def plot_vehicule_evolution(df_commune, model, region=None, departement=None, commune=None, carburant=None, crit_air=None, depart_Annee=None, fin_Annee=None):
 
-    # Transformation du dfFrame en un format long en utilisant la fonction melt()
-    df = df.melt(id_vars=['region_de_residence',
+    # Transformation du df_communeFrame en un format long en utilisant la fonction melt()
+    df_commune = df_commune.melt(id_vars=['region_de_residence',
                           'departement_de_residence',
                           'commune_de_residence',
                           'carburant',
@@ -164,12 +164,12 @@ def plot_vehicule_evolution(df, model, region=None, departement=None, commune=No
                  value_vars=[str(Annee)
                              for Annee in range(depart_Annee, fin_Annee+1)],
                  var_name=[('Annee')], value_name='nombre de véhicules')
-    print ('df_melted',df)
-    # df.to_csv(p + r'/France_data/df_melted_utf-8_b.csv',encoding="utf-8")
+    # print ('df_commune_melted',df_commune)
+    # df_commune.to_csv(p + r'/France_df_commune_unique/df_commune_melted_utf-8_b.csv',encoding="utf-8")
     
 
 
-    l_col = df.columns
+    l_col = df_commune.columns
     l_Annees = []
 
     for element in l_col:
@@ -185,44 +185,44 @@ def plot_vehicule_evolution(df, model, region=None, departement=None, commune=No
         region_str = 'toutes régions'
     else:
         list_region = region.split(',')
-        df = df[df['region_de_residence'].isin(list_region)]
+        df_commune = df_commune[df_commune['region_de_residence'].isin(list_region)]
         region_str = ",".join(list_region)
-        df0 = df.iloc[0]
-        departement_str = str(df0.loc['departement_de_residence'])
-        commune_str = str(df0.loc['commune_de_residence'])
+        df_legende = df_commune.iloc[0]
+        departement_str = str(df_legende.loc['departement_de_residence'])
+        commune_str = str(df_legende.loc['commune_de_residence'])
         
     if departement == None:
         departement_str = 'tous départements'
     else:
         list_departement = departement.split(',')
-        df = df[df['departement_de_residence'].isin(list_departement)]
+        df_commune = df_commune[df_commune['departement_de_residence'].isin(list_departement)]
         departement_str = ",".join(list_departement)
-        df0 = df.iloc[0]
-        region_str = str(df0.loc['region_de_residence'])
-        commune_str = str(df0.loc['commune_de_residence'])
+        df_legende = df_commune.iloc[0]
+        region_str = str(df_legende.loc['region_de_residence'])
+        commune_str = str(df_legende.loc['commune_de_residence'])
         
     if commune == None:
         commune_str = 'toutes communes'
     else:
         list_commune = commune.split(',')
-        df = df[df['commune_de_residence'].isin(list_commune)]
+        df_commune = df_commune[df_commune['commune_de_residence'].isin(list_commune)]
         commune_str = ",".join(list_commune)
-        df0 = df.iloc[0]
-        region_str = str(df0.loc['region_de_residence'])
-        departement_str = str(df0.loc['departement_de_residence'])
+        df_legende = df_commune.iloc[0]
+        region_str = str(df_legende.loc['region_de_residence'])
+        departement_str = str(df_legende.loc['departement_de_residence'])
 
     if carburant == None:
         carburant_str = 'tous carburants'
     else:
         list_carburant = carburant.split(',')
-        df = df[df['carburant'].isin(list_carburant)]
+        df_commune = df_commune[df_commune['carburant'].isin(list_carburant)]
         carburant_str = ",".join(list_carburant)
 
     if crit_air == None:
         crit_air_str = "tous Crit'Air"
     else:
         list_crit_air = crit_air.split(',')
-        df = df[df['crit_air'].isin(list_crit_air)]
+        df_commune = df_commune[df_commune['crit_air'].isin(list_crit_air)]
         crit_air_str = ",".join(list_crit_air)
 
     if depart_Annee is None and fin_Annee is None:
@@ -238,37 +238,37 @@ def plot_vehicule_evolution(df, model, region=None, departement=None, commune=No
     
 
     # Conversion de la colonne Annee en entier
-    # df['Annee'] = df['Annee'].astype(int)
+    # df_commune['Annee'] = df_commune['Annee'].astype(int)
 
-    # df.groupby([model])['nombre de véhicules'].sum().plot.pie(autopct='%1.1f%%')
+    # df_commune.groupby([model])['nombre de véhicules'].sum().plot.pie(autopct='%1.1f%%')
 
-    # df = df.groupby(['Annee', model, 'geo_point_2d', 'lattitude', 'longitude'])['nombre de véhicules'].sum().reset_index()
-    df = df.groupby([ model,'Annee', 'departement_de_residence','commune_de_residence','geo_point_2d', 'geo_shape', 'lattitude', 'longitude'])[
+    # df_commune = df_commune.groupby(['Annee', model, 'geo_point_2d', 'lattitude', 'longitude'])['nombre de véhicules'].sum().reset_index()
+    df_commune = df_commune.groupby([ model,'Annee', 'departement_de_residence','commune_de_residence','geo_point_2d', 'geo_shape', 'lattitude', 'longitude'])[
         'nombre de véhicules'].agg(['sum', 'mean', 'min', 'max']).reset_index()
-    df = df.rename(columns={'sum': 'nombre de véhicules'})
+    df_commune = df_commune.rename(columns={'sum': 'nombre de véhicules'})
 
-    df2 = df.groupby([model,'Annee'])['nombre de véhicules'].agg(['sum','max']).reset_index()
-    df2 = df2.rename(columns={'sum': 'nombre de véhicules'})
+    df_dept = df_commune.groupby([model,'Annee'])['nombre de véhicules'].agg(['sum','max']).reset_index()
+    df_dept = df_dept.rename(columns={'sum': 'nombre de véhicules'})
  
-    # print(df)
+    # print(df_commune)
 
-    # df['max'] = df['max'].astype(int)
-    # df0 = df.dropna(subset=['max'])
-    df = df.loc[(df['max'] != 0)]
-    df2 = df2.loc[(df2['max'] != 0)]
-    print (df2)
-    # df = df.drop(['commune_de_residence','geo_point_2d','geo_shape','lattitude','longitude','mean', 'min', 'max'], axis=1)
-    # print ('drop_com',df)
+    # df_commune['max'] = df_commune['max'].astype(int)
+    # df_legende = df_commune.dropna(subset=['max'])
+    df_commune = df_commune.loc[(df_commune['max'] != 0)]
+    df_dept = df_dept.loc[(df_dept['max'] != 0)]
+    # print (df_dept)
+    # df_commune = df_commune.drop(['commune_de_residence','geo_point_2d','geo_shape_commune','lattitude','longitude','mean', 'min', 'max'], axis=1)
+    # print ('drop_com',df_commune)
     
 
     # Tri des données par les colonnes Annee et model
-    # df = df.sort_values(by=['Annee', model])
-    df = sort_par_model_couleur(df,model)
-    df2 = sort_par_model_couleur(df2,model)
-    print(df2)
+    # df_commune = df_commune.sort_values(by=['Annee', model])
+    # df_commune = sort_par_model_couleur(df_commune,model)
+    df_dept = sort_par_model_couleur(df_dept,model)
+    print(df_dept)
 
-    # df.to_csv(p + r'/France_df/df_MIN_MAX.csv', encoding="utf-8")
-    # print(df)
+    # df_commune.to_csv(p + r'/France_df_commune/df_commune_MIN_MAX.csv', encoding="utf-8")
+    # print(df_commune)
 
     # Agrandissement du cadre du graphique
 
@@ -276,32 +276,32 @@ def plot_vehicule_evolution(df, model, region=None, departement=None, commune=No
     plt.figure(figsize=(15, 8.5))
 
     # Création d'un graphique en ligne en utilisant la bibliothèque seaborn
-    # sns.lineplot(df=df, x='Annee', y='nombre de véhicules', hue=model)
-    # sns.barplot(df=df, x='Annee', y='nombre de véhicules', hue=model)
+    # sns.lineplot(df_commune=df_commune, x='Annee', y='nombre de véhicules', hue=model)
+    # sns.barplot(df_commune=df_commune, x='Annee', y='nombre de véhicules', hue=model)
     # sns.cubehelix_palette(start=2, rot=0, dark=0, light=.95, reverse=True, as_cmap=True)
 
-    # ax = sns.barplot(df=df, x='Annee', y='nombre de véhicules',palette="mako", hue=model)
+    # ax = sns.barplot(df_commune=df_commune, x='Annee', y='nombre de véhicules',palette="mako", hue=model)
     # palette = ax.palette('mako').reversed()
     # inverted_palette = sns.color_palette(palette)
-    # data0 = df2[df2[model] == model]
-    # pc = df2['couleur']
-    pc = pd.unique(df2['couleur'])
-    print (pc)
+    # df_commune_unique0 = df_dept[df_dept[model] == model]
+    # pc = df_dept['couleur']
+    pc = pd.unique(df_dept['couleur'])
+    # print (pc)
 
-    sns.barplot(data=df2, x='Annee', y='nombre de véhicules',
+    sns.barplot(data=df_dept, x='Annee', y='nombre de véhicules',
                 palette=pc, hue=model)
     
-    # sns.barplot(data=df, x='Annee', y='nombre de véhicules',
+    # sns.barplot(df_commune_unique=df_commune, x='Annee', y='nombre de véhicules',
     #             palette=colors, hue=model)
 
-    # sns.barplot(df=df, x='Annee', y='nombre de véhicules',palette="mako", hue=model)#, ax=ax0)
+    # sns.barplot(df_commune=df_commune, x='Annee', y='nombre de véhicules',palette="mako", hue=model)#, ax=ax0)
 
     # ax0.set_ylabel("Sequential")
 
     # ax1 = ax0.twinx()
-    # sns.boxplot(df=df, x='Annee', y='nombre de véhicules',palette="rocket", hue=model, ax=ax1)
+    # sns.boxplot(df_commune=df_commune, x='Annee', y='nombre de véhicules',palette="rocket", hue=model, ax=ax1)
 
-    # sns.barplot(df=df, x='Annee', y='nombre de véhicules', hue=model)
+    # sns.barplot(df_commune=df_commune, x='Annee', y='nombre de véhicules', hue=model)
 
     # plt.gca().yaxis.set_ticks(range(0, 100, 10), minor = True)
 
@@ -339,20 +339,14 @@ def plot_vehicule_evolution(df, model, region=None, departement=None, commune=No
     
 
     # Calculate percentage of vehicles in each category
-    df['proportion'] = df.groupby('commune_de_residence')['nombre de véhicules'].apply(lambda x: x / x.sum() * 100)
-    
-    # df2.to_csv(p + r'/France_data/df_proportion2.csv',encoding="utf-8")
+    df_commune['proportion'] = df_commune.groupby('commune_de_residence')['nombre de véhicules'].apply(lambda x: x / x.sum() * 100)
+    print (df_commune)
 
-
-    # df['proportion2'] = df.groupby('crit_air')['nombre de véhicules'].sum()
-    # df['proportion'] = df['nombre de véhicules'] / df.groupby(['commune_de_residence', 'crit_air'])['nombre de véhicules'].transform('sum') * 100
-    # df.to_csv(p + r'/France_data/df_proportion.csv',encoding="utf-8")
-    print (df)
-
-    # Sort df by Crit'Air category
-    df = sort_par_model_couleur(df,model)
-    # df.to_csv(p + r'/France_data/df_proportion.csv',encoding="utf-8")
-    carto(df,model)
+    # Sort df_commune by Crit'Air category
+    df_commune = sort_par_model_couleur(df_commune,model)
+    print (df_commune)
+    # df_commune.to_csv(p + r'/France_data/df_commune_proportion.csv',encoding="utf-8")
+    carto(df_commune,model)
 
     
 
@@ -373,6 +367,6 @@ def plot_vehicule_evolution(df, model, region=None, departement=None, commune=No
 #     plot_vehicule_evolution(df_propre,'carburant',r,None,None,None,None,None,2021)
 # plot_vehicule_evolution(df_propre,'crit_air',None,None,'Menucourt','Hybride rechargeable,Electrique et hydrogène',None,None,None)
 plot_vehicule_evolution(df_propre, 'crit_air', None,
-                        'Hauts-de-Seine', None, None, None, 2021, 2021)
+                        "Yvelines", None, None, None, 2021, 2021)
 # plot_vehicule_evolution(df_propre, 'crit_air', 'Auvergne-Rhône-Alpes',
 #                         None, None, None, None, 2020, 2021)
